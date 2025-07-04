50907cfa9c1f31c47df1f9aee279e8f22d817bc3
camel-1984 <rostmolchanov@mail.ru>
2025-07-04 02:42:17 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+ymaps.ready(function () {
+    var map = new ymaps.Map("map", {
+        center: [54.723776, 20.536536],
+        zoom: 10
+    });
+    var placemark = new ymaps.Placemark([54.723776, 20.536536], {
+        balloonContent: 'Офис ГазСпецСтрой'
+    });
+    map.geoObjects.add(placemark);
+});
+
+function openContactModal() {
+    document.getElementById("contactModal").style.display = "block";
+}
+function closeContactModal() {
+    document.getElementById("contactModal").style.display = "none";
+}
+window.addEventListener("click", function (e) {
+    const modal = document.getElementById("contactModal");
+    if (e.target === modal) modal.style.display = "none";
+});
