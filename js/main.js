13a36e29931fb559eec404119782308e28897c63
camel-1984 <rostmolchanov@mail.ru>
2025-07-08 19:05:03 +0200

    08.07

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-ymaps.ready(function () {
-    var map = new ymaps.Map("map", {
-        center: [54.723776, 20.536536],
-        zoom: 10
+ymaps.ready(init);
+
+function init() {
+    const map = new ymaps.Map("map", {
+        center: [54.710426, 20.509279],
+        zoom: 8
     });
-    var placemark = new ymaps.Placemark([54.723776, 20.536536], {
-        balloonContent: 'Офис ГазСпецСтрой'
+
+    const locations = [
+        { name: "Калининград, ул. Римская 31", coords: [54.710426, 20.509279] },
+        { name: "Гурьевск, пр. Ленина 10", coords: [54.7748, 20.6056] },
+        { name: "Светлогорск, ул. Центральная 5", coords: [54.9399, 20.1514] },
+        { name: "Балтийск, ул. Морская 3", coords: [54.6511, 19.9067] },
+        { name: "Черняховск, ул. Победы 11", coords: [54.6304, 21.8168] },
+        { name: "Советск, ул. Трудовая 18", coords: [55.0819, 21.8857] },
+        { name: "Пионерский, ул. Набережная 7", coords: [54.9511, 20.2322] },
+        { name: "Гусев, ул. Пушкина 40", coords: [54.5921, 22.1995] }
+    ];
+
+    locations.forEach(loc => {
+        const placemark = new ymaps.Placemark(loc.coords, {
+            balloonContent: `<strong>${loc.name}</strong>`
+        });
+        map.geoObjects.add(placemark);
     });
-    map.geoObjects.add(placemark);
-});
+}
+
 
 function openContactModal() {
     document.getElementById("contactModal").style.display = "block";
@@ -37,6 +54,8 @@ barba.init({
         }
     }]
 });
+
+
 
 
 
